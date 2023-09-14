17821b647be1211e48b0bcf7686fbe3b0e4a17af
Declan Moran <dec1@geoaze.com>
2023-09-14 21:16:15 +0200

    schemathesis

--- kotlin/src/main/kotlin/com/example/plugins/Routing.kt
+++ kotlin/src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,8 +5,13 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
 
-data class Customer(val name: String, val age: Int)
+@Serializable
+data class Customer(val name: String? = null, val age: Int? = null)
 
 fun Application.configureRouting() {
     routing {
@@ -21,16 +26,36 @@ fun Application.configureRouting() {
             }
             call.respondText("Hello $n")
         }
-        get("/qstr/") {
-            val ct = call.request.contentType()
-            val p =  call.request.queryParameters["p4"]?.toInt()
-            call.respondText("Hello test}")
+        get("/qstr") {
+            //val ct = call.request.contentType()
+            val p1 : Int?  =  call.parameters["my_p1"]?.toIntOrNull()
+            val p2 : Int? =  call.parameters["my_p2"]?.toIntOrNull()
+            call.respondText("Hello test $p1")
         }
 
         post("/customer") {
-            val cust = call.receive<Customer>()
-            call.respondText("Customer  $cust.name, $cust.age received", status = HttpStatusCode.Created)
-        }
+//            println(call.request.headers.getAll("Content-Type")
+            val payload = call.receiveText().takeIf { it.isNotBlank() } ?: "{}"
 
+            println(".....Received payload: $payload")
+            //val cust1 = Json.decodeFromString<Customer>(payload)
+            //println(".......cust1: $cust1")
+            //call.respondText("Customer  $cust1.name, $cust1.age received", status = HttpStatusCode.Created)
+
+            var cust: Customer? = null
+            try {
+                //cust = call.receive<Customer>()
+                cust = Json.decodeFromString<Customer>(payload)
+//            } catch (e: SerializationException) {
+//                println("....BadRequest - Malformed JSON payload")
+//                call.respond(HttpStatusCode.BadRequest, "Malformed JSON payload")
+            } catch (ex: Exception) {
+                println("....Other exception - cause: ${ex.cause}, message: ${ex.message}")
+                call.respond(HttpStatusCode.BadRequest, "General exception")
+            }
+            println("....OK - trying to repond with customer")
+                // val cust = call.receive<Customer>()
+            call.respondText("Customer  $cust?.name, $cust?.age received", status = HttpStatusCode.Created)
+        }
     }
 }
--- python/client.py
+++ python/client.py
@@ -1,50 +1,38 @@
 import yaml
 import requests
 import pandas as pd
-import os
+
+# https://schemathesis.readthedocs.io/en/
+
+#def tst_client2():
+
+import schemathesis
 
 
 
+import yaml
+import json
+from hypothesis import settings, Phase
+from twisted.python.util import println
 
-# ---------------------------------------
-def tst_client():
-    url_base = "http://localhost:8080"
+path_dir = "/Users/declan/Documents/zone/low/kotlin/open_api/kotlin/src/main/resources/openapi/"
+path_yaml = path_dir + "documentation.yaml"
+# path_json = path_dir + "documentation.json"
+# with open(path_yaml, 'r') as yaml_file:
+#     yaml_data = yaml.safe_load(yaml_file)
+#
+# with open(path_json, 'w') as json_file:
+#     json.dump(yaml_data, json_file)
 
-    current_script_dir = os.path.dirname(os.path.abspath(__file__))
-    filepath_spec = os.path.join(current_script_dir, '../', 'kotlin/src/main/resources/openapi/documentation.yaml')
-    # "/Users/declan/Documents/zone/low/kotlin/ktor/src/main/resources/openapi/documentation.yaml"
-    # Load and parse OpenAPI spec
+schema = schemathesis.from_path(path_yaml,  base_url="http://localhost:8080")
 
-    with open(filepath_spec, 'r') as f:
-        spec_dict = yaml.safe_load(f)
-
-
-    df = pd.DataFrame()
-    # Loop through API paths and methods
-    for path, path_item in spec_dict['paths'].items():
-        for method, operation in path_item.items():
-            # print(f"calling: {method} {path}")
-            url = f"{url_base}{path}"
-
-            res = requests.request(method, url)  # Add (operation.)parameters as needed
-            # eg for get request, operation.parameters is a list of dicts eg . could discover (name, type) of each
-            passed  = res.status_code == 200
-
-            row = pd.DataFrame({'Pass': passed, 'Method': method, 'Path': path, 'Status_Code': res.status_code,
-                                'Response_Text': res.text[:100]}, index=[0])  # First 100 characters of response; customize as needed
-            df = pd.concat([df, row], ignore_index=True)
+@schemathesis.hook
+def before_call(context, case):
+    println(case.query) # = {"q": "42"}
 
 
 
-        # df_fail = df[df['Pass'] == False].copy()
-
-    df = df.sort_values(by='Pass', ascending=True)
-
-    num_failed = len(df[df['Pass'] == False])
-    print("-----------------------------")
-    print(f"Failed {num_failed} of {len(df)} tests")
-    print("-----------------------------")
-    print(df.to_markdown())
-
-# ---------------------------------------
-tst_client()
+@settings(max_examples=10)
+@schema.parametrize()
+def test_api(case):
+    case.call_and_validate()
